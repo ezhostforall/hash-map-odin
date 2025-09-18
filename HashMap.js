5e81a7dbe8f1e24f82f330cd1479cf72fde372e3
ezhostforall <117274922+ezhostforall@users.noreply.github.com>
2025-09-18 20:20:50 +0000

    Refactor has method in HashMap to use node properties for clarity

--- HashMap.js
+++ HashMap.js
@@ -86,8 +86,8 @@ class HashMap {
     const bucket = this.buckets[index];
     if (!bucket) return false;
 
-    for (let [k] of bucket) {
-      if (k === key) {
+    for (let node of bucket) {
+      if (node.key === key) {
         return true;
       }
     }
