b9e53a07b4795783081c845fc309e231769b5763
ezhostforall <117274922+ezhostforall@users.noreply.github.com>
2025-09-18 20:03:43 +0000

    Implement values method in HashMap to retrieve all values

--- HashMap.js
+++ HashMap.js
@@ -138,4 +138,16 @@ class HashMap {
     }
     return keys;
   }
+
+  values() {
+    const values = [];
+    for (const bucket of this.buckets) {
+      if (bucket) {
+        for (const [, value] of bucket) {
+          values.push(value);
+        }
+      }
+    }
+    return values;
+  }
 }
